e1664aa7306a05535f26d3bf76bfbde464cab434
moonbrooke <smoeland@gmail.com>
2026-05-18 08:39:53 +0700

    data: update

---
+++ data.js
@@ -0,0 +1,82 @@
+const campData = [
+    {
+        "title": "Hammock Camping Under Bridge",
+        "url": "https://www.youtube.com/watch?v=AWMHmASqivM",
+        "description": "I had an amazing camping trip! I secretly set up a #hammock under a bridge on a remote road. There was a store close by selling step 2's and a variety of grocery items I used to form a #campingmeal. I slept pretty well, aside from some occasional road noise.",
+        "stealth": true,
+        "date": "2023/09/22",
+        "location": "44.053383911846296, -66.161722815107",
+        "city": "Salmon River, NS",
+        "note": ""
+    },
+    {
+        "title": "Stealth Camping Behind Sign",
+        "url": "https://www.youtube.com/watch?v=lblyg-NOVjE",
+        "description": "This was one for the ages! I finally got out to go camping behind a sign. It took quite a while to find the perfect sign, but it was worth it! There was no view from the highway eastbound lanes and very minimal exposure on the westbound lanes. Basic backpack and general gear. I had a hammock, to use as a bivvy in case of rain, but the weather was pretty good.",
+        "stealth": true,
+        "date": "2020/10/02",
+        "location": "53.0176608039331, -112.87689806170151",
+        "city": "Salmon River, NS",
+        "note": ""
+    },
+    {
+        "title": "Police Station Stealth Camping In Hammock",
+        "url": "https://www.youtube.com/watch?v=yMIA6zdUVCA",
+        "description": "Well, I finally found my dream police station to camp behind. That was a strange sentence. When we traveled through after buying the school bus beautiful wife noticed a fantastic spot behind the police station. I gathered up my courage and my camping gear and headed into the woods there for a great stealth camping adventure in small-town Alberta.",
+        "stealth": true,
+        "date": "2021/09/06",
+        "location": "53.94948185223092, -113.12309247232461",
+        "city": "Redwater, AB",
+        "note": ""
+    },
+    {
+        "title": "Stealth Camping Beside Fire Station",
+        "url": "https://www.youtube.com/watch?v=OeUXopzBOBc",
+        "description": "I brought out my small camo tent and snuck into the woods beside a small fire hall to camp for the night. It was an interesting #camping location with some excitement throughout the night. It was a bit noisy, but it was much better than camping under a bridge. #Hunkerdown!",
+        "stealth": true,
+        "date": "2023/12/01",
+        "location": "49.338892766266824, -124.44130184133331",
+        "city": "Qualicum Beach, BC",
+        "note": ""
+    },
+    {
+        "title": "Welcome Sign Stealth Camping",
+        "url": "https://www.youtube.com/watch?v=dQ9eImcE9cA",
+        "description": "I went stealth camping in a small plot of trees behind a welcome sign. I've been trying to camp behind this sign for quite some time and finally, the timing and weather worked out for me! Was a decent night, road noise is expected of course but it made for an interesting night!",
+        "stealth": true,
+        "date": "2023/04/28",
+        "location": "48.90281474019962, -123.72084919855675",
+        "city": "Chemainus, BC",
+        "note": ""
+    },
+    {
+        "title": "Parking Garage Stealth Camping",
+        "url": "https://www.youtube.com/watch?v=hBnlPd__Vg0",
+        "description": "I camped for the night hidden in my SUV in a parking garage, or parkade depending on what you call them. I needed to apply for my passport and the lines are notoriously long unless you're there first thing in the morning. Let's be honest, first thing in the morning is not my friend so I decided to camp out to be there bright and early. The garage was quite hot, but the camping was nice and stealthy.",
+        "stealth": true,
+        "date": "2024/06/07",
+        "location": "53.545021870257095, -113.49814503263497",
+        "city": "Edmonton, AB",
+        "note": ""
+    },
+    {
+        "title": "Disguised Truck Stealth Camping",
+        "url": "https://www.youtube.com/watch?v=DYuAxGxH-8E",
+        "description": "A makeshift camper disguised as landscaping debris made a great little camping spot for the night. I camped out in a small town by a lake. The camper was just made with 4 pallets, 2 boards, 3 tarps, and some rachet straps. I crawled into the camper from the back of the truck and had a small mattress and some cooking equipment for a quick and easy meal.",
+        "stealth": true,
+        "date": "2024/08/16",
+        "location": "53.67693471104305, -114.35484398041937",
+        "city": "Alberta Beach, AB",
+        "note": ""
+    },
+    {
+        "title": "Business Park Roundabout Stealth Camping",
+        "url": "https://www.youtube.com/watch?v=UTDpgEraPbA",
+        "description": "I found another roundabout to camp in!! These are always so fun, and I generally don't like to do the same idea twice but this was TOO tempting. Almost like it was a trap to catch me stealth camping. It was close by to a hotel I stayed at the night before, stones throw from a Cabelas and a Princess Auto. Made some pad Thai and hunkered down for the night on a beautiful autumn evening.",
+        "stealth": true,
+        "date": "2024/10/28",
+        "location": "45.297252297023, -75.94231119063862",
+        "city": "Ottawa, ON",
+        "note": ""
+    },
+];
